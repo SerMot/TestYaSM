513d27913561f535b9e28c772677668b79dc478e
SerMot <41292432+SerMot@users.noreply.github.com>
2018-07-19 09:41:10 +0300

    Update FirstTest.java
    Добавление PageObject и страницы создания и закрытия драйвера

--- src/test/java/ru/Ya/FirstTest.java
+++ src/test/java/ru/Ya/FirstTest.java
@@ -1,49 +1,15 @@
-package ru.Ya;
+package ru.Ya;     
+        import org.testng.annotations.Test;
+        import static org.testng.Assert.assertTrue;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.assertTrue;
-
-public class FirstTest {
-
-    private static WebDriver driver;
-
-    @BeforeClass
-    public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "/Users/SMotrokhov/IdeaProjects/testya/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://ya.ru/");
-        String title = driver.getTitle();
-        assertTrue(title.equals("Яндекс"));
-    }
+public class FirstTest extends CreateCloseDriver {
 
     @Test
     public void checkTest() {
-        WebElement inputWeatherField = driver.findElement(By.cssSelector("input[id='text']"));
-        inputWeatherField.clear();
-        inputWeatherField.sendKeys("погода пенза");
-        WebElement buttonFind = driver.findElement(By.cssSelector("button[type='submit']"));
-        buttonFind.click();
-        WebElement pogodaAssert = driver.findElement(By.cssSelector("li[data-cid="0"] div[class='organic__url-text']"));
-        String actualString = pogodaAssert.getText();
-        assertTrue(actualString.contains("Погода"));
-    }
+        mainPage.inputWeatherField("Погода Пенза");
+        mainPage.clickFindButton();
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null)
-            driver.quit();
+        String actualString = mainPage.getWeatherLink();
+        assertTrue(actualString.contains("Погода"));
     }
 }
