bb18a434e928c9431c8e050e23725d3633c4482b
SerMot <41292432+SerMot@users.noreply.github.com>
2018-07-24 07:29:16 +0300

    Update FirstTest.java
    Добавлен URL в тест

--- src/test/java/ru/Ya/Test/FirstTest.java
+++ src/test/java/ru/Ya/Test/FirstTest.java
@@ -1,13 +1,15 @@
 package ru.Ya.Test;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.assertTrue;
 
 public class FirstTest extends ru.Ya.CreateCloseDriver {
 
     @Test
     public void checkTest() {
+        String URL = "https://yandex.by";
+        mainPage.navigate(URL);
 
-        driver.get("https://ya.ru/");
         mainPage.inputWeatherField("Погода Пенза");
         mainPage.clickFindButton();
 
